b6d1881ada1fdee0044bfb5f7aeb25ee936b7c93
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-05 01:34:01 +0630

    Add docstrings, update exception suite and debug

--- week03/broken_score.py
+++ week03/broken_score.py
@@ -3,8 +3,10 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+
 def main():
-    score = float(input("Enter score: "))
+    """Score Performance Checker Program"""
+    score = get_valid_input()
     while score < 0 or score > 100:
         print("Invalid score")
         score = get_valid_input()
@@ -12,17 +14,19 @@ def main():
 
 
 def get_valid_input():
+    """Prevent error raised by blank line"""
     value_input = False
     while not value_input:
         try:
             num = float(input("Enter score: "))
             value_input = True
             return num
-        except:
+        except ValueError:
             print("Enter a valid input.")
 
 
 def determine_category(num):
+    """Determine the score performance"""
     if num < 50:
         return "Bad"
     elif 50 <= num < 90:
