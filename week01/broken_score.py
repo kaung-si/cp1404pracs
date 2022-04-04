d57a55556df5c7755a6e9a22c57f70c3f2de9949
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-05 01:34:41 +0630

    Add docstrings, update exception suite and debug

--- week01/broken_score.py
+++ week01/broken_score.py
@@ -4,13 +4,15 @@ Broken program to determine score status
 """
 
 
-score = float(input("Enter score: "))
-while score < 0 or score > 100:
-    print("Invalid score")
-    score = float(input("Enter score: "))
-if score < 50:
-    print("Bad")
-elif 50 <= score < 90:
-    print("Passable")
-else:
-    print("Excellent")
+# score = float(input("Enter score: "))
+# while score < 0 or score > 100:
+#     print("Invalid score")
+#     score = float(input("Enter score: "))
+# if score < 50:
+#     print("Bad")
+# elif 50 <= score < 90:
+#     print("Passable")
+# else:
+#     print("Excellent")
+
+
