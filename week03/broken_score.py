6198f9234609ddfbf0b77ad4180a8c8a79f1c099
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-05 01:08:34 +0630

    Modify program structure, add input validation function

--- week03/broken_score.py
+++ week03/broken_score.py
@@ -3,16 +3,33 @@ CP1404/CP5632 - Practical
 Broken program to determine score status
 """
 
+def main():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        print("Invalid score")
+        score = get_valid_input()
+    print(determine_category(score))
 
-# score = float(input("Enter score: "))
-# while score < 0 or score > 100:
-#     print("Invalid score")
-#     score = float(input("Enter score: "))
-# if score < 50:
-#     print("Bad")
-# elif 50 <= score < 90:
-#     print("Passable")
-# else:
-#     print("Excellent")
 
+def get_valid_input():
+    value_input = False
+    while not value_input:
+        try:
+            num = float(input("Enter score: "))
+            value_input = True
+            return num
+        except:
+            print("Enter a valid input.")
+
+
+def determine_category(num):
+    if num < 50:
+        return "Bad"
+    elif 50 <= num < 90:
+        return "Passable"
+    else:
+        return "Excellent"
+
+
+main()
 
