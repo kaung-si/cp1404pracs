667538913de848bcbcf2c6af60e0b60a2d204c15
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-16 10:45:04 +0630

    Add practice and extension work

--- week04/list_exercises.py
+++ week04/list_exercises.py
@@ -14,4 +14,24 @@ def main():
           .format(numbers[0], numbers[-1], min(numbers), max(numbers), sum(numbers) / len(numbers)))
 
 
-main()
+def main_extended():
+    """Start Program"""
+    value_input = False
+    counter = 1
+    while not value_input:
+        num = int(input(f"Number {counter}: "))
+        while num > 0:
+            value_input = True
+            numbers.append(num)
+            counter += 1
+            num = int(input(f"Number {counter}: "))
+    print("The first number is {}"
+          "The last number is {}"
+          "\nThe smallest number is {}"
+          "\nThe largest number is {}"
+          "\nThe average of the numbers is {}"
+          .format(numbers[0], numbers[-1], min(numbers), max(numbers), sum(numbers) / len(numbers)))
+
+
+# main()
+main_extended()
