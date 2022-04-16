57721c1c7f614ea0b10a7d4b74f8fe81158d5a3d
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-16 12:45:39 +0630

    Add week05 practicals

---
+++ week05/state_names.py
@@ -0,0 +1,20 @@
+"""
+CP1404/CP5632 Practical
+State names in a dictionary
+File needs reformatting
+"""
+
+CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
+                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
+print(CODE_TO_NAME)
+
+for state_name in CODE_TO_NAME:
+    print("{:<3} is {:<}".format(state_name, CODE_TO_NAME[state_name]))
+
+state_code = input("Enter short state: ").upper()
+while state_code != "":
+    if state_code in CODE_TO_NAME:
+        print(state_code, "is", CODE_TO_NAME[state_code])
+    else:
+        print("Invalid short state")
+    state_code = input("Enter short state: ").upper()
