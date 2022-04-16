cf3a137d32aa770fb1d868a3d8e8d5c7dea05e1d
kaung-si <kaungsithu.nyle@gmail.com>
2022-04-16 10:45:44 +0630

    Use enumerate function to display report

--- week04/total_income.py
+++ week04/total_income.py
@@ -12,8 +12,8 @@ def main():
     for month in range(1, num_month + 1):
         income = float(input("Enter income for month {}: ".format(month)))
         incomes.append(income)
-    display_report(incomes, num_month)
-
+    # display_report(incomes, num_month)
+    display_report_with_enumerate(incomes)
 
 def display_report(incomes, num_month):
     """Display report in a formatted way."""
@@ -23,6 +23,15 @@ def display_report(incomes, num_month):
         income = incomes[month - 1]
         total += income
         print("Month {:2} - Income: ${:10.2f}\t\tTotal: ${:10.2f}".format(month, income, total))
+
+
+def display_report_with_enumerate(incomes):
+    """Display report in a formatted way."""
+    print("\nIncome Report\n-------------")
+    total = 0
+    for index, income in enumerate(incomes):
+        total += income
+        print("Month {:2} - Income: ${:10.2f}\t\tTotal: ${:10.2f}".format(index + 1, income, total))
 
 
 main()
